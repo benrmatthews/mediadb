9197f79a1aa4debec213a326bd251582d2759306
benrmatthews <ben@montfort.io>
2014-11-22 20:50:29 +0000

    added credit card field to signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,17 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
-
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+ 
+  protected
+ 
+  def configure_permitted_parameters
+  	#This allows the attributes to be accessible at sign up. I had to add email and password after adding token. 
+  	devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :stripe_card_token, :email, :password) }
+  	devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :stripe_card_token) }
+  end
+  
   private
 
   def user_not_authorized
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,28 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  attr_accessor :stripe_card_token
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   enum role: [:user, :vip, :admin]
   after_initialize :set_default_role, :if => :new_record?
+ 
+         validates :name, presence: true
+         after_create :create_a_customer
+ 
+         def create_a_customer
+              token = self.stripe_card_token
 
-  def set_default_role
-    self.role ||= :user
-  end
-
+              customer = Stripe::Customer.create(
+              :card => token,
+              :plan => 120,
+              :email => self.email
+          )       	
+			#Need to note that User cc has gone through
+         end
+ 
+        def set_default_role
+          self.role ||= :user
+        end   
+ 
 end
